6208ac1353c2a0a2490e891a2f92e56e66481e21
Charlie <2023TromC28@moundsviewschools.org>
2023-01-28 14:18:35 -0600

    add dynamic auto

--- src/main/java/frc/robot/auto/AutoFactory.java
+++ src/main/java/frc/robot/auto/AutoFactory.java
@@ -9,13 +9,8 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 
-import java.util.function.Supplier;
-
-
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.wpi.first.math.controller.PIDController;
@@ -31,11 +26,21 @@ import frc.robot.subsystems.DrivetrainSubsystem;
 public class AutoFactory {
     private final DrivetrainSubsystem drivetrain;
 
+    private Pose2d lastPose;
+
     public AutoFactory(DrivetrainSubsystem drivetrain) {
         this.drivetrain = drivetrain;
     }
 
-    public SequentialCommandGroup getAuto(Grid grid, Node node, Channel channel) {
+    public SequentialCommandGroup getAuto(
+        Grid grid, 
+        Node node, 
+        Channel channel, 
+        GamePiece gamePiece,
+        boolean endChargeStation
+    ) {
+        SequentialCommandGroup command = new SequentialCommandGroup();
+
         double robotLength = (Constants.Drivetrain.DRIVETRAIN_WHEELBASE_METERS + (2 * Constants.Auto.BUPPER_DEPTH_METERS));
 
         double startingXMeters = Units.inchesToMeters((3.5 + 16.5) + (3 * grid.ordinal() + node.ordinal()) * (18.5 + 13.5));
@@ -57,12 +62,12 @@ public class AutoFactory {
 
                 initialMidPoint = new Translation2d(
                     leftChannelXMeters,
-                    Constants.Auto.DISTANCE_CHARGE_STATION_GRID_METETERS / 2
+                    Constants.Auto.DISTANCE_GRID_TO_CHARGE_STATION_METERS / 2
                 );
 
                 initialEndPose = new Pose2d(
                     leftChannelXMeters,
-                    0,
+                    endingYMeters,
                     Rotation2d.fromDegrees(0)
                 );
                 break;
@@ -72,12 +77,12 @@ public class AutoFactory {
 
                 initialMidPoint = new Translation2d(
                     rightChannelXMeters,
-                    Constants.Auto.DISTANCE_CHARGE_STATION_GRID_METETERS / 2
+                    Constants.Auto.DISTANCE_GRID_TO_CHARGE_STATION_METERS / 2
                 );
 
                 initialEndPose = new Pose2d(
                     rightChannelXMeters,
-                    0,
+                    endingYMeters,
                     Rotation2d.fromDegrees(0)
                 );
                 break;
@@ -89,8 +94,58 @@ public class AutoFactory {
             initialEndPose,
             new Rotation2d(180)
         );
+        command.addCommands(initialSwerveCommand);
 
-        return null;
+        if (gamePiece != GamePiece.NO_GAME_PIECE) {
+            Pose2d gamePieceEndPose = new Pose2d(
+                (gamePiece.ordinal() * Constants.Auto.DISTANCE_BETWEEN_GAME_PIECES_METERS) + Constants.Auto.DISTANCE_WALL_TO_GAME_PIECE_METERS,
+                Constants.Auto.DISTANCE_GRID_TO_GAME_PIECES_METERS,
+                new Rotation2d()
+            );
+
+            SwerveControllerCommand gamePieceSwerveCommand = createSwerveCommand(
+                lastPose, 
+                new ArrayList<Translation2d>(),
+                gamePieceEndPose, 
+                new Rotation2d()
+            );
+            command.addCommands(gamePieceSwerveCommand);
+        }
+
+        if (endChargeStation){
+            Translation2d farEntryPoint = new Translation2d(
+                Constants.Auto.COMMUNITY_WIDTH_METERS / 2,
+                Constants.Auto.DISTANCE_GRID_TO_CHARGE_STATION_METERS + Constants.Auto.CHARGE_STATION_DEPTH
+            );
+
+            Translation2d nearEntryPoint = new Translation2d(
+                Constants.Auto.COMMUNITY_WIDTH_METERS / 2,
+                Constants.Auto.DISTANCE_GRID_TO_CHARGE_STATION_METERS
+            );
+
+            Translation2d entryPoint = null;
+
+            Translation2d lastTranslation = new Translation2d(lastPose.getX(), lastPose.getY());
+            if (lastTranslation.getDistance(farEntryPoint) > lastTranslation.getDistance(nearEntryPoint)) {
+                entryPoint = nearEntryPoint;
+            } else {
+                entryPoint = farEntryPoint;
+            }
+
+            SwerveControllerCommand endChargeStationSwerveCommand = createSwerveCommand(
+                lastPose,
+                List.of(entryPoint),
+                new Pose2d(
+                    Constants.Auto.COMMUNITY_WIDTH_METERS / 2,
+                    Constants.Auto.DISTANCE_GRID_TO_CHARGE_STATION_METERS + (Constants.Auto.CHARGE_STATION_DEPTH/2),
+                    new Rotation2d()
+                ),
+                new Rotation2d()
+            );
+            command.addCommands(endChargeStationSwerveCommand);
+        } 
+
+        return command;
     }
 
     private SwerveControllerCommand createSwerveCommand(
@@ -112,6 +167,8 @@ public class AutoFactory {
             0,
             new TrapezoidProfile.Constraints(Math.PI, Math.PI)
         );
+
+        lastPose = endPose;
 
         return new SwerveControllerCommand(
             TrajectoryGenerator.generateTrajectory(
@@ -146,5 +203,13 @@ public class AutoFactory {
     public static enum Channel {
         LEFT_CHANNEL, 
         RIGHT_CHANNEL;
+    }
+
+    public static enum GamePiece {
+        FAR_LEFT_GAME_PIECE,
+        MIDDLE_LEET_GAME_PIECE,
+        MIDDLE_RIGHT_GAME_PIECE,
+        FAR_RIGHT_GAME_PIECE,
+        NO_GAME_PIECE;
     }
 }
