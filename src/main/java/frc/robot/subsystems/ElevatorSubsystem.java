99c76e8e08b45c75ae1ceede404a6db79b7c435b
Matt Huesman <45442854+matt-huesman@users.noreply.github.com>
2023-01-29 19:17:55 -0600

    Syntax fix

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
@@ -8,17 +9,25 @@ import frc.robot.Constants;
 
 
 public class ElevatorSubsystem extends SubsystemBase {
-    private TalonFX beltMotor = new TalonFX(Constants.Elevator.CAN_ID);
+    private TalonFX beltMotor;
+
+    public ElevatorSubsystem() {
+        beltMotor = new TalonFX(Constants.Elevator.CAN_ID);
+        beltMotor.setNeutralMode(NeutralMode.Brake);
+    }
 
     public void elevatorPos1() {
         // beltMotor.set(TalonFXControlMode.MotionMagic, );
     }
+
     public void elvUp() {
         beltMotor.set(TalonFXControlMode.PercentOutput, 0.1);
     }
+
     public void stop() {
         beltMotor.set(TalonFXControlMode.PercentOutput, 0);
     }
+
     public void elvDown() {
         beltMotor.set(TalonFXControlMode.PercentOutput, -0.1);
     }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.fasterxml.jackson.annotation.JsonFormat.Value;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -22,6 +21,7 @@ public class Intake extends SubsystemBase {
   }  
   public void reverseIntake() {
     intakeMotor.set(VictorSPXControlMode.PercentOutput, -0.1);
+  }
 
   public void stop() {
     intakeMotor.set(VictorSPXControlMode.PercentOutput, 0);
