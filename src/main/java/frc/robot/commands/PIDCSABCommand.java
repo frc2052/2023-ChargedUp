50e9c8e17324ffc9441bf21b6db40c57a57b8e0a
Caleb <caleb.fynewever@gmail.com>
2023-01-31 22:58:08 -0600

    PID Balancing Command work continued

--- src/main/java/frc/robot/commands/PIDCSABCommand.java
+++ src/main/java/frc/robot/commands/PIDCSABCommand.java
@@ -1,34 +1,46 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleConsumer;
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
-import frc.robot.Constants.Drivetrain;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class PIDCSABCommand extends PIDCommand {
-  private  DrivetrainSubsystem drivetrain;
-  private double angle = drivetrain.getNavx().getPitch();
-  private double speed;
+  private static DrivetrainSubsystem drivetrain;
 
-  /** Creates a new PIDCSABCommand. */
+  private final static double kP = 0.05;
+  private final static double kI = 0.05;
+  private final static double kD = 0.05;
+
   public PIDCSABCommand(
-  ) {
-    super(
-        // The controller that the command will use
-        new PIDController(0, 0, 0),
-        // This should return the measurement
-        () -> 0,
-        // This should return the setpoint (can also be a constant)
-        () -> 0,
-        // This uses the output
-        output -> {
-          // Use the output here
-        });
-
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(this.drivetrain);
-    // Configure additional PID options by calling `getController` here.
+    PIDController CSABcontroller,
+    DoubleSupplier measurementSource,
+    DoubleSupplier setpointSource,
+    DoubleConsumer useOutput,
+    DrivetrainSubsystem requirements) {
+      super(
+          // The controller that the command will use
+          new PIDController(kP, kI, kD),
+          // This should return the measurement
+          () -> 0,
+          // This should return the setpoint (can also be a constant)
+          () -> 0,
+          // This uses the output
+          output -> drivetrain.drive(new ChassisSpeeds(output, 0, 0))
+            // Use the output here
+          );
+      // Use addRequirements() here to declare subsystem dependencies.
+      addRequirements(drivetrain);
+      // Configure additional PID options by calling `getController` here.
+      getController().setTolerance(1);
+      getController().setSetpoint(0);
+      getController().calculate(drivetrain.getNavx().getPitch());
   }
+
+
 
   // Returns true when the command should end.
   @Override
