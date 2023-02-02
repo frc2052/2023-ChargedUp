88f712549b3e38757c8101310b43e0479fb4487c
caleb.fynewever <2026fynec65@moundsviewschools.org>
2023-02-01 18:26:38 -0600

    PID Loop is built, just need to calibrate

--- src/main/java/frc/robot/commands/PIDCSABCommand.java
+++ src/main/java/frc/robot/commands/PIDCSABCommand.java
@@ -9,35 +9,39 @@ import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class PIDCSABCommand extends PIDCommand {
-  private static DrivetrainSubsystem drivetrain;
+  private final DrivetrainSubsystem drivetrain;
 
   private final static double kP = 0.05;
-  private final static double kI = 0.05;
-  private final static double kD = 0.05;
+  private final static double kI = 0.0;
+  private final static double kD = 0.0;
 
   public PIDCSABCommand(
     PIDController CSABcontroller,
     DoubleSupplier measurementSource,
     DoubleSupplier setpointSource,
     DoubleConsumer useOutput,
-    DrivetrainSubsystem requirements) {
+    DrivetrainSubsystem drivetrain
+  ) {
       super(
-          // The controller that the command will use
-          new PIDController(kP, kI, kD),
-          // This should return the measurement
-          () -> 0,
-          // This should return the setpoint (can also be a constant)
-          () -> 0,
-          // This uses the output
-          output -> drivetrain.drive(new ChassisSpeeds(output, 0, 0))
-            // Use the output here
-          );
+        // The controller that the command will use
+        new PIDController(kP, kI, kD),
+        // This should return the measurement
+        () -> drivetrain.getNavx().getPitch(),
+        // This should return the setpoint (can also be a constant)
+        () -> 0,
+        // This uses the output
+        output -> drivetrain.drive(new ChassisSpeeds(output, 0, 0))
+          // Use the output here
+      );
+
+      this.drivetrain = drivetrain;
+
       // Use addRequirements() here to declare subsystem dependencies.
       addRequirements(drivetrain);
       // Configure additional PID options by calling `getController` here.
       getController().setTolerance(1);
       getController().setSetpoint(0);
-      getController().calculate(drivetrain.getNavx().getPitch());
+      //getController().calculate(drivetrain.getNavx().getPitch());
   }
 
 
