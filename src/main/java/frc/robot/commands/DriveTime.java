3418af3ff76da96e83cc7ae5b7e24f9bc5c66f4d
anika <2026livsa81@moundsviewschools.org>
2023-02-04 15:36:21 -0600

    Lynnea A, Anika L, Charles T, ForwardAuto

---
+++ src/main/java/frc/robot/commands/DriveTime.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class DriveTime extends CommandBase {
+
+  private double speed;
+  private double duration;
+  private DrivetrainSubsystem drive;
+  
+  /** Creates a new DriveTime. */
+  public DriveTime(double speed, double time, DrivetrainSubsystem drivetrain) {
+    this.speed = speed;
+    this.duration = time * 1000;
+    this.drive = drive;
+    addRequirements(drive);
+  }
+  private double startTime;
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    startTime = System.currentTimeMillis();
+    drive.drive(0.0, 0.5, 0.0, true);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    drive.drive(0.0, 0.5, 0.0, true);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drive.drive(0.0, 0.5, 0.0, true);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return (System.currentTimeMillis() - startTime) >= duration;
+  }
+}
