ad476f7a4702f2a5014746016e614869426ea49b
Matt <matt.huesman@team2052.com>
2023-02-11 09:40:28 -0600

    Fixed swerve offsets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,22 +26,22 @@ public final class Constants {
         public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 12;
         public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 11;
         public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 10;
-        public static final double FRONT_LEFT_MODULE_STEER_OFFSET_RADIANS = 4.335 - (Math.PI / 2);
+        public static final double FRONT_LEFT_MODULE_STEER_OFFSET_RADIANS = 4.335 + (Math.PI / 2);
 
         public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
         public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 2;
         public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 1;
-        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET_RADIANS = 4.858 - (Math.PI / 2);
+        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET_RADIANS = 4.858 + (Math.PI / 2);
 
         public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 9;
         public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8;
         public static final int BACK_LEFT_MODULE_STEER_ENCODER = 7;
-        public static final double BACK_LEFT_MODULE_STEER_OFFSET_RADIANS = 1.612 - (Math.PI / 2);
+        public static final double BACK_LEFT_MODULE_STEER_OFFSET_RADIANS = 1.612 + (Math.PI / 2);
 
         public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6;
         public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 5;
         public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 4;
-        public static final double BACK_RIGHT_MODULE_STEER_OFFSET_RADIANS = 1.179 - (Math.PI / 2);
+        public static final double BACK_RIGHT_MODULE_STEER_OFFSET_RADIANS = 1.179 + (Math.PI / 2);
     }
 
     public static final class Dashboard {
