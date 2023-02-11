d700b75e05a9f1f607f77010e571c93a5110bd42
anika <2026livsa81@moundsviewschools.org>
2023-02-11 11:02:29 -0600

    Intake current limiting Co-authored-by: Lynneaalberts <Lynneaalberts@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,8 +4,10 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
@@ -14,27 +16,37 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-  /** Creates a new Intake. */
-  public Intake() {}
   private DoubleSolenoid intakesolenoide = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
 
-  private VictorSPX intakeMotor = new VictorSPX(Constants.Intake.INTAKE_MOTOR_PWM_PORT);
+  private TalonSRX intakeMotor = new TalonSRX(Constants.Intake.INTAKE_MOTOR_PWM_PORT);
+
+
+  /** Creates a new Intake. */
+  public Intake() {
+
+    intakeMotor.configPeakCurrentLimit(6); // don't activate current limit until current exceeds 30 A ...
+    intakeMotor.configPeakCurrentDuration(100); // ... for at least 100 ms
+    intakeMotor.configContinuousCurrentLimit(5); // once current-limiting is actived, hold at 20A
+    intakeMotor.enableCurrentLimit(true); 
+
+  }
 
   public void intake() {
     intakesolenoide.set(Value.kForward);
-    intakeMotor.set(VictorSPXControlMode.PercentOutput, 0.1);
+    intakeMotor.set(ControlMode.PercentOutput, 0.1);
   }  
   public void reverseIntake() {
     intakesolenoide.set(Value.kForward);
-    intakeMotor.set(VictorSPXControlMode.PercentOutput,  -0.1); }
+    intakeMotor.set(ControlMode.PercentOutput,  -0.1); }
     
   public void stop() {
     intakesolenoide.set(Value.kReverse);
-    intakeMotor.set(VictorSPXControlMode.PercentOutput, 0);
-    
-
-  
-
+    intakeMotor.set(ControlMode.PercentOutput, 0);
   }
   
+ 
+  
+
+
+
 }
